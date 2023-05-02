363d5df15357cae3fb5ea882da2f1161d93c99b6
David Hewitt <davidmhewitt@users.noreply.github.com>
2023-05-02 18:16:28 +0000

    Decompress XML before parse

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
+use flate2::read::GzDecoder;
+use libxml::parser::ParserOptions;
 use reqwest::StatusCode;
-use std::time::Duration;
+use std::{io::Read, time::Duration};
 use tokio::task::spawn_blocking;
 use tokio_stream::StreamExt;
 use tokio_util::io::StreamReader;
@@ -33,12 +35,22 @@ async fn main() -> std::io::Result<()> {
             };
 
             if res.status() != StatusCode::OK {
+                tracing::error!(
+                    "Flatpak remote returned {} for appstream.xml.gz",
+                    res.status()
+                );
                 continue;
             }
 
             let mut out_file = match tokio::fs::File::create("/tmp/appstream.xml.gz").await {
                 Ok(f) => f,
-                Err(_) => continue,
+                Err(e) => {
+                    tracing::error!(
+                        "Error creating temporary file for appstream download: {:?}",
+                        e
+                    );
+                    continue;
+                }
             };
 
             let stream = res.bytes_stream().map(|result| {
@@ -49,20 +61,47 @@ async fn main() -> std::io::Result<()> {
 
             match tokio::io::copy(&mut read, &mut out_file).await {
                 Ok(_) => {}
-                Err(_) => continue,
+                Err(e) => {
+                    tracing::error!("Error downloading appstream file: {:?}", e);
+                    continue;
+                }
             }
 
             match spawn_blocking(|| -> Result<(), std::io::Error> {
+                let file = std::fs::File::open("/tmp/appstream.xml.gz")?;
+
+                let mut xml_data = Vec::new();
+                let mut decoder = GzDecoder::new(file);
+                decoder.read_to_end(&mut xml_data)?;
+
                 let parser = libxml::parser::Parser::default();
                 let appstream_xml = parser
-                    .parse_file("/tmp/appstream.xml.gz")
+                    .parse_string(
+                        String::from_utf8(xml_data)
+                            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?,
+                    )
                     .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
 
+                let root = match appstream_xml.get_root_element() {
+                    Some(r) => r,
+                    None => {
+                        return Err(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            "Couldn't get root element from AppStream XML",
+                        ))
+                    }
+                };
+
                 Ok(())
-            }).await {
+            })
+            .await
+            {
                 Ok(r) => match r {
-                    Ok(_) => {},
-                    Err(_) => continue,
+                    Ok(_) => {}
+                    Err(e) => {
+                        tracing::error!("Error parsing appstream file: {:?}", e);
+                        continue;
+                    }
                 },
                 Err(_) => continue,
             };
