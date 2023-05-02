2577c424ec8a9535f287a59597e416a9db866063
David Hewitt <davidmhewitt@users.noreply.github.com>
2023-05-02 09:06:38 +0000

    Start appstream download and parsing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,9 @@
+use reqwest::StatusCode;
+use std::time::Duration;
+use tokio::task::spawn_blocking;
+use tokio_stream::StreamExt;
+use tokio_util::io::StreamReader;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
@@ -10,6 +16,58 @@ async fn main() -> std::io::Result<()> {
     let application = backend::startup::Application::build(settings, None).await?;
 
     tracing::event!(target: "backend", tracing::Level::INFO, "Listening on http://127.0.0.1:{}/", application.port());
+
+    tokio::spawn(async {
+        let mut interval = tokio::time::interval(Duration::from_secs(60 * 30));
+        let client = reqwest::Client::new();
+        loop {
+            interval.tick().await;
+            tracing::info!("Updating AppStream info");
+            let res = match client
+                .get("https://flatpak.elementary.io/repo/appstream/x86_64/appstream.xml.gz")
+                .send()
+                .await
+            {
+                Ok(r) => r,
+                Err(_) => continue,
+            };
+
+            if res.status() != StatusCode::OK {
+                continue;
+            }
+
+            let mut out_file = match tokio::fs::File::create("/tmp/appstream.xml.gz").await {
+                Ok(f) => f,
+                Err(_) => continue,
+            };
+
+            let stream = res.bytes_stream().map(|result| {
+                result.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))
+            });
+
+            let mut read = StreamReader::new(stream);
+
+            match tokio::io::copy(&mut read, &mut out_file).await {
+                Ok(_) => {}
+                Err(_) => continue,
+            }
+
+            match spawn_blocking(|| -> Result<(), std::io::Error> {
+                let parser = libxml::parser::Parser::default();
+                let appstream_xml = parser
+                    .parse_file("/tmp/appstream.xml.gz")
+                    .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+                Ok(())
+            }).await {
+                Ok(r) => match r {
+                    Ok(_) => {},
+                    Err(_) => continue,
+                },
+                Err(_) => continue,
+            };
+        }
+    });
 
     application.run_until_stopped().await?;
     Ok(())
