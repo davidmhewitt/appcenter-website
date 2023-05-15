6697441996860e94d4f236b01352b9371511b860
David Hewitt <davidmhewitt@users.noreply.github.com>
2023-05-15 09:13:08 +0000

    Fix static generation of locales

--- frontend/app/[lang]/layout.tsx
+++ frontend/app/[lang]/layout.tsx
@@ -4,7 +4,7 @@ import '../globals.css'
 import Header from '../../components/Header'
 
 export async function generateStaticParams() {
-  return locales.map((lng) => ({ lng }))
+  return locales.map((lang) => ({ lang }))
 }
 
 export default function RootLayout({
