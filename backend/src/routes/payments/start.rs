4afedda6e4e4b944d7476dd49a58c50ad91c97d2
David Hewitt <davidmhewitt@gmail.com>
2023-05-27 23:55:37 +0000

    Hook up some connect stuff

--- backend/src/routes/payments/start.rs
+++ backend/src/routes/payments/start.rs
@@ -2,64 +2,86 @@ use actix_web::{post, HttpResponse};
 use stripe::{
     CheckoutSession, CheckoutSessionMode, Client, CreateCheckoutSession,
     CreateCheckoutSessionLineItems, CreateCheckoutSessionLineItemsPriceData,
-    CreateCheckoutSessionLineItemsPriceDataProductData, Currency, Expandable,
+    CreateCheckoutSessionLineItemsPriceDataProductData, CreateCheckoutSessionPaymentIntentData,
+    CreateCheckoutSessionPaymentIntentDataTransferData, Currency, Metadata,
 };
+
+use crate::types::payments::AppPaymentRequest;
 
 #[post("/start")]
 #[cfg_attr(
     not(coverage),
     tracing::instrument(name = "starting a payment", skip(stripe_client))
 )]
-pub async fn start(stripe_client: actix_web::web::Data<Client>) -> HttpResponse {
-    // finally, create a checkout session for this product / price
-    let checkout_session = {
-        let mut params = CreateCheckoutSession::new("http://test.com/success");
-        params.cancel_url = Some("http://test.com/cancel");
-        params.mode = Some(CheckoutSessionMode::Payment);
-        params.line_items = Some(vec![CreateCheckoutSessionLineItems {
-            quantity: Some(3),
-            price_data: Some(CreateCheckoutSessionLineItemsPriceData {
-                currency: Currency::USD,
-                unit_amount: Some(300),
-                product_data: Some(CreateCheckoutSessionLineItemsPriceDataProductData {
-                    name: "Test Product".into(),
-                    ..Default::default()
-                }),
+pub async fn start(
+    stripe_client: actix_web::web::Data<Client>,
+    app_information: actix_web::web::Json<AppPaymentRequest>,
+) -> HttpResponse {
+    let mut params = CreateCheckoutSession::new("http://test.com/success");
+    params.cancel_url = Some("http://test.com/cancel");
+    params.mode = Some(CheckoutSessionMode::Payment);
+    params.line_items = Some(vec![CreateCheckoutSessionLineItems {
+        quantity: Some(1),
+        price_data: Some(CreateCheckoutSessionLineItemsPriceData {
+            currency: Currency::USD,
+            unit_amount: Some(app_information.amount.into()),
+            product_data: Some(CreateCheckoutSessionLineItemsPriceDataProductData {
+                name: app_information.app_name.to_owned(),
                 ..Default::default()
             }),
             ..Default::default()
-        }]);
-        params.expand = &["line_items", "line_items.data.price.product"];
+        }),
+        ..Default::default()
+    }]);
+    params.metadata = Some(Metadata::from([(
+        "app_id".into(),
+        app_information.app_id.to_owned(),
+    )]));
+    params.payment_intent_data = Some(CreateCheckoutSessionPaymentIntentData {
+        application_fee_amount: Some(calculate_fee(app_information.amount).into()),
+        transfer_data: Some(CreateCheckoutSessionPaymentIntentDataTransferData {
+            destination: app_information.stripe_connect_id.to_owned(),
+            ..Default::default()
+        }),
+        on_behalf_of: Some(app_information.stripe_connect_id.to_owned()),
+        ..Default::default()
+    });
 
-        CheckoutSession::create(&stripe_client, params)
-            .await
-            .unwrap()
+    let checkout_session = match CheckoutSession::create(&stripe_client, params).await {
+        Ok(s) => s,
+        Err(e) => {
+            tracing::error!("Error creating stripe checkout session: {}", e);
+            return HttpResponse::InternalServerError().finish();
+        }
     };
 
-    println!(
-        "created a {} checkout session for {} {:?} for {} {} at {}",
-        checkout_session.payment_status,
-        checkout_session.line_items.data[0].quantity.unwrap(),
-        match checkout_session.line_items.data[0]
-            .price
-            .as_ref()
-            .unwrap()
-            .product
-            .as_ref()
-            .unwrap()
-        {
-            Expandable::Object(p) => p.name.as_ref().unwrap(),
-            _ => panic!("product not found"),
-        },
-        checkout_session.amount_subtotal.unwrap() / 100,
-        checkout_session.line_items.data[0]
-            .price
-            .as_ref()
-            .unwrap()
-            .currency
-            .unwrap(),
-        checkout_session.url.unwrap()
-    );
+    HttpResponse::SeeOther()
+        .insert_header((
+            actix_web::http::header::LOCATION,
+            checkout_session.url.unwrap(),
+        ))
+        .finish()
+}
 
-    HttpResponse::Ok().finish()
+fn calculate_fee(amount: u32) -> u32 {
+    let fee = ((amount as f64) * 0.3).round() as u32;
+    if fee >= 50 {
+        fee
+    } else {
+        50
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fee_calculations() {
+        assert_eq!(50, calculate_fee(100));
+        assert_eq!(50, calculate_fee(150));
+        assert_eq!(51, calculate_fee(170));
+        assert_eq!(60, calculate_fee(200));
+        assert_eq!(150, calculate_fee(500));
+    }
 }
---
+++ backend/src/types/payments.rs
@@ -0,0 +1,22 @@
+use serde::Deserialize;
+#[cfg(feature = "openapi")]
+use utoipa::ToSchema;
+
+#[derive(Deserialize, Debug)]
+#[cfg_attr(feature = "openapi", derive(ToSchema))]
+pub struct AppPaymentRequest {
+    #[cfg_attr(feature = "openapi", schema(example = "Torrential"))]
+    pub app_name: String,
+    #[cfg_attr(
+        feature = "openapi",
+        schema(example = "com.github.davidmhewitt.torrential")
+    )]
+    pub app_id: String,
+    #[cfg_attr(feature = "openapi", schema(example = 300))]
+    pub amount: u32,
+    #[cfg_attr(
+        feature = "openapi",
+        schema(example = "pk_live_Uhb96elhovWNRGkq07m2FRO9008Ia8OtVa")
+    )]
+    pub stripe_connect_id: String,
+}
