f103c6544d471177e232e0748120270c20c665b0
David Hewitt <davidmhewitt@users.noreply.github.com>
2023-08-01 07:37:31 +0000

    Set namespace on login translations

--- frontend/app/[lang]/login/page.tsx
+++ frontend/app/[lang]/login/page.tsx
@@ -17,7 +17,7 @@ export default async function Login({
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            {t('page-title')}
+            {t('page-title', { ns: 'login' })}
           </h2>
         </div>
 
@@ -29,14 +29,14 @@ export default async function Login({
             <TextBox
               name="email"
               autoComplete="email"
-              title={t('form-labels.email-address')}
+              title={t('form-labels.email-address', { ns: 'login' })}
             />
 
             <TextBox
               name="password"
               type="password"
               autoComplete="current-password"
-              title={t('form-labels.password')}
+              title={t('form-labels.password', { ns: 'login' })}
             />
 
             <div>
@@ -44,7 +44,7 @@ export default async function Login({
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                {t('form-labels.sign-in-button')}
+                {t('form-labels.sign-in-button', { ns: 'login' })}
               </button>
             </div>
           </form>
@@ -61,16 +61,16 @@ export default async function Login({
             role="button"
           >
             <Image className="w-4 mx-2" src={githubLogo} alt="GitHub Logo" />
-            {t('github-login')}
+            {t('github-login', { ns: 'login' })}
           </Link>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            {t('no-account')}&nbsp;
+            {t('no-account', { ns: 'login' })}&nbsp;
             <Link
               href="/register"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
-              {t('register-action')}
+              {t('register-action', { ns: 'login' })}
             </Link>
           </p>
         </div>
--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // this will check Typescript files
-  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
-
   // This will lint and format TypeScript and                                             //JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
     `npx eslint --fix ${filenames.join(' ')}`,
